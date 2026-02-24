b1a1e565f2b62e5c57f9394384fe20eebea5350b
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-02-25 01:09:16 +0500

    Initial Commit

---
+++ Bandmates/BandmatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BandmatesApp.swift
+//  Bandmates
+//
+//  Created by Mac mini on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BandmatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
