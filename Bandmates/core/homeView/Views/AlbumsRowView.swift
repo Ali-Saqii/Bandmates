6fc031cde42d90c9b7fe8ea3924d59d3beac67b4
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-22 22:09:23 +0500

    [feature] home album rows

--- Bandmates/core/homeView/Views/AlbumsRowView.swift
+++ Bandmates/core/homeView/Views/AlbumsRowView.swift
@@ -8,11 +8,82 @@
 import SwiftUI
 
 struct AlbumsRowView: View {
+    let albumImage: String
+    let albumName:String
+    let artistName: String
+    let ratingCount: Double
+    let totalRatingcount: Int
+    @Binding var isAlBumSaved: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            AsyncImage(url: URL(string:albumImage)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 74, height: 78)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                    
+                } else if phase.error != nil {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .frame(width: 74, height: 78)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                    
+                } else {
+                    ProgressView()
+                        .frame(width: 40, height: 40)
+                }
+            }.padding(.leading,7)
+            VStack(alignment:.leading,spacing:6) {
+                Text(albumName)
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                HStack(spacing:5) {
+                    Image("artist")
+                        .tint(Color.background)
+                    Text(artistName)
+                        .font(.system(size: 13))
+                        .foregroundStyle(Color.background)
+                }.frame(height: 16)
+                HStack(spacing:5) {
+                    Image(systemName: "star.fill")
+                        .font(.caption)
+                        .foregroundStyle(.yellow)
+                    Text("\(ratingCount)")
+                        .font(.system(size: 13))
+                        .foregroundStyle(Color.background)
+                    Text("(\(totalRatingcount) ratings)")
+                        .font(.system(size: 10))
+                        .foregroundStyle(Color.background)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 16)
+            }
+            
+        }.frame(maxWidth: .infinity,alignment: .leading)
+            .frame(height: 98)
+            .background(.white)
+            .clipShape(
+                RoundedRectangle(cornerRadius: 12)
+            )
+            .overlay(alignment: .trailing, content: {
+                albumSaveButton(isSaved: isAlBumSaved) {
+                    withAnimation(.easeIn){
+                        isAlBumSaved.toggle()
+                    }
+                }.padding(.trailing,20)
+            })
+            .shadow(color: Color.textfieldcolor.opacity(0.7), radius: 5)
+            .padding(.horizontal)
     }
 }
 
 #Preview {
-    AlbumsRowView()
+    AlbumsRowView(albumImage: "https://images.unsplash.com/photo-1517841905240-472988babdf9?w=400",
+                  albumName: "",
+                  artistName: "",
+                  ratingCount: 4.5,
+                  totalRatingcount: 89, isAlBumSaved: .constant(true)
+)
 }
