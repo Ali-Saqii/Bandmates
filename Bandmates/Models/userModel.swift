e7a3bc4f3aafd233a4396a7376b40f90b7745f98
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-22 22:01:01 +0500

    [feature] user model

--- Bandmates/Models/userModel.swift
+++ Bandmates/Models/userModel.swift
@@ -1,8 +1,20 @@
 //
-//  userViewModel.swift
+//  userModel.swift
 //  Bandmates
 //
 //  Created by Mac mini on 18/03/2026.
 //
 
 import Foundation
+import Combine
+
+struct userModel: Codable ,Identifiable {
+    var id  = UUID().uuidString
+    let profileImage : String
+    let fullName: String
+    let userName: String
+    let Bio: String
+    let waiting: Int
+    let totalBandmates: Int
+    let toralSavedAlbums: Int
+}
