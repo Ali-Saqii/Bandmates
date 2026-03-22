f374afcde480ecf82ddde3ca613f772140add27b
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-22 22:12:01 +0500

    [Build] Home recently play albums view

--- Bandmates/core/homeView/Views/recentlyPlayedView.swift
+++ Bandmates/core/homeView/Views/recentlyPlayedView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct recentlyPlayedView: View {
+    let image: String
+    let albumName: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                AsyncImage(url: URL(string:image)) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 102, height: 85)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                        
+                    } else if phase.error != nil {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .frame(width: 102, height: 85)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                        
+                    } else {
+                        ProgressView()
+                            .frame(width: 40, height: 40)
+                    }
+                }
+            }
+            .frame(width: 122, height: 105)
+            .background(.white)
+            .clipShape(
+                RoundedRectangle(cornerRadius: 18)
+            )
+            .shadow(color: Color.textfieldcolor.opacity(0.7), radius: 5)
+            Text(albumName)
+                .font(.system(size: 14,weight: .bold))
+        }
     }
 }
 
 #Preview {
-    recentlyPlayedView()
+    recentlyPlayedView(image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400", albumName: "jkgjkfd jfdj")
 }
