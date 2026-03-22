be76b78c2ab2672bac1c4b2b69a7bf31c7d003d8
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-22 22:08:45 +0500

    [feature] reuse able feature rows

--- Bandmates/core/components/featuresRowView.swift
+++ Bandmates/core/components/featuresRowView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct featuresRowView: View {
+    let text : String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Circle()
+                .fill(Color.background)
+                .frame(width: 17, height: 17)
+                .overlay {
+                    Image(systemName: "checkmark")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                }
+            Text(text)
+                .font(.system(size: 15))
+        }
     }
 }
 
 #Preview {
-    featuresRowView()
+    featuresRowView(text: "fjjh fjfhjhf dsfhjdfh")
 }
