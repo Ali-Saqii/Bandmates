f53b6976ea7524eb7fff1657bbb8cb6a9fe0de12
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-22 22:06:19 +0500

    [feature] bandmate request button

--- Bandmates/core/components/buttons/requestButton.swift
+++ Bandmates/core/components/buttons/requestButton.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct requestButton: View {
+    let title: String
+    let title2: String
+    let action: () -> Void
+    let height: CGFloat
+    let width: CGFloat
+    let font: Font
+    let cornerRadius: CGFloat
+    let isRequested: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            action()
+        } label: {
+            Text(isRequested ? title2 :title)
+                .font(.caption2)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .frame(height: height)
+                .frame(width: width)
+        }.background(isRequested ? Color.gray.opacity(0.8) : Color.background)
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+
     }
 }
 
 #Preview {
-    requestButton()
+    requestButton(title: "requested", title2: "Waiting to Jam", action: {}, height: 30, width: 100, font: .caption2, cornerRadius: 25, isRequested: true)
 }
