b5d79690251a3a438af8a2619b661a149dd5dd4e
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-23 19:00:38 +0500

    menu icons

---
+++ Bandmates/core/tabView/helpAndSupport.swift
@@ -0,0 +1,18 @@
+//
+//  helpAndSupport.swift
+//  Bandmates
+//
+//  Created by Mac mini on 23/03/2026.
+//
+
+import SwiftUI
+
+struct helpAndSupport: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    helpAndSupport()
+}
