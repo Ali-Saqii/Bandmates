555f789cdee16924ef50ef920c75c4cd35ba646c
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-25 01:12:55 +0500

    [build] BandmatesViewHeader

--- Bandmates/core/homeView/Views/BandmatesView/views/BandematesViewHeader.swift
+++ Bandmates/core/homeView/Views/BandmatesView/views/BandematesViewHeader.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct BandematesViewHeader: View {
+    @Binding var selectedTab: BandmatesviewTabs
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing:0) {
+            ForEach(BandmatesviewTabs.allCases, id: \.rawValue) { tab in
+                BandmatesHeaderButtons(
+                    tab:tab ,
+                    isSelected: selectedTab == tab,
+                    action: {
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                            selectedTab = tab
+                        }
+                    },
+                    height: 38,
+                    cornerRadius: 16)
+            }
+        }.frame(maxWidth: .infinity)
+            .frame(height: 38)
+            .background(.textfieldcolor)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .padding()
     }
 }
 
 #Preview {
-    BandematesViewHeader()
+    BandematesViewHeader(selectedTab: .constant(BandmatesviewTabs.myBand))
 }
