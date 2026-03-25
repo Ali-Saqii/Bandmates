8d0d84383a84af8f0832f9a19f77c1183cde7fbf
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-26 03:20:01 +0500

    initilising following views

--- Bandmates/core/profile/view/profileView.swift
+++ Bandmates/core/profile/view/profileView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct profileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white
+                .ignoresSafeArea(.all)
+            VStack {
+                Text("profile")
+            }
+        }
     }
 }
 
