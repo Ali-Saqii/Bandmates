cc20df3dfb672839ec1ee39b8c08331b2e3dc806
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-28 04:05:06 +0500

    [ui Build] collection albums row View

--- Bandmates/core/collections/Views/albumsRowView.swift
+++ Bandmates/core/collections/Views/albumsRowView.swift
@@ -8,11 +8,85 @@
 import SwiftUI
 
 struct albumsRowView: View {
+    let album: albumModel
+    let ButtonAction: () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            AsyncImage(url: URL(string:album.image)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 74, height: 78)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                    
+                } else if phase.error != nil {
+                    Image("placeholderImage")
+                        .frame(width: 74, height: 78)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 8)
+                        ).background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(
+                                    LinearGradient(colors: [Color.orange,Color.pink], startPoint: .topLeading, endPoint: .bottomTrailing)
+                                )
+                                .strokeBorder(Color.background,lineWidth: 2)
+                        )
+                    
+                } else {
+                    ProgressView()
+                        .frame(width: 74, height: 78)
+                }
+            }.padding(.leading,7)
+            VStack(alignment:.leading,spacing:6) {
+                Text(album.albumName)
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                HStack(spacing:5) {
+                    Image("artist")
+                        .tint(Color.background)
+                    Text(album.albumArtistName)
+                        .font(.system(size: 13))
+                        .foregroundStyle(Color.background)
+                }.frame(height: 16)
+                HStack(spacing:5) {
+                    Image(systemName: "star.fill")
+                        .font(.caption)
+                        .foregroundStyle(.yellow)
+                    Text("\(album.averageRating , specifier: "%.1f")")
+                        .font(.system(size: 13))
+                        .foregroundStyle(Color.background)
+                    Text("(\(album.totalRatingCount) ratings)")
+                        .font(.system(size: 10))
+                        .foregroundStyle(Color.background)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(height: 16)
+            }
+            
+        }.frame(maxWidth: .infinity,alignment: .leading)
+            .frame(height: 98)
+            .background(.white)
+            .clipShape(
+                RoundedRectangle(cornerRadius: 12)
+            )
+            .shadow(color: .textfieldcolor.opacity(0.5), radius: 12)
+            .overlay(alignment:.trailing) {
+                Image(systemName: "trash.fill")
+                    .foregroundStyle(.white)
+                    .padding(.all,8)
+                    .onTapGesture {
+                        ButtonAction()
+                    }
+                    .background(
+                        Circle()
+                            .fill(.red)
+                    )
+                    .padding(.trailing)
+            }
+            
     }
 }
 
 #Preview {
-    albumsRowView()
+    albumsRowView(album:DeveloperPreview.instance.album, ButtonAction: {print("fdjkfh dsfds")})
 }
