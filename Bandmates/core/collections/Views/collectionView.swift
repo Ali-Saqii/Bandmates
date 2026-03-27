c7783cca650e91a9ea655d2dfda504ad9a856fe4
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-27 19:41:37 +0500

    [Build] collectionView

--- Bandmates/core/collections/Views/collectionView.swift
+++ Bandmates/core/collections/Views/collectionView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct collectionView: View {
+    @EnvironmentObject var homeVm : HomeViewModel
+    @State private var searchText = ""
     var body: some View {
         ZStack {
             Color.white
                 .ignoresSafeArea(.all)
-            VStack {
-                Text("collectionView")
+            VStack() {
+                HStack(spacing:10) {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundStyle(.gray)
+                        .padding(.leading)
+                        .onTapGesture {
+                            print(searchText)
+                        }
+                    TextField("Search Any Collection here ...", text: $searchText)
+                }.frame(maxWidth:.infinity)
+                    .frame(height: 60)
+                    .background(Color.textfieldcolor.opacity(0.7))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal,20)
+                ScrollView{
+                    VStack(spacing:15) {
+                        ForEach(homeVm.user.Collections) { collection in
+                            CollectiomRowView(Collection: collection, editAction: {print("edit")}, DeleteActin: {print("delete")})
+                        }
+                    }.padding(.horizontal)
+                }.scrollIndicators(.hidden)
+                    .scrollBounceBehavior(.basedOnSize)
+                    .padding(.top)
             }
         }
     }
 }
 
+
 #Preview {
     collectionView()
+        .environmentObject(HomeViewModel())
 }
