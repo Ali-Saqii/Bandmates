3e6eaf2a211171399601c297104167d33e945e2e
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-28 04:05:45 +0500

    [ui Update] collectionView

--- Bandmates/core/collections/Views/collectionView.swift
+++ Bandmates/core/collections/Views/collectionView.swift
@@ -10,6 +10,14 @@ import SwiftUI
 struct collectionView: View {
     @EnvironmentObject var homeVm : HomeViewModel
     @State private var searchText = ""
+    @State private var showAddCollection = false
+    @State private var showEditCollection = false
+    @State private var DeleteCollection = false
+    @State private var CollectionName = ""
+    @State private var CollectionDescriptione = ""
+    @State private var CollectionNewName = ""
+    @State private var CollectionNewDescriptione = ""
+    @State private var selesctedCollection: CollectionModel? = nil
     var body: some View {
         ZStack {
             Color.white
@@ -31,14 +39,96 @@ struct collectionView: View {
                 ScrollView{
                     VStack(spacing:15) {
                         ForEach(homeVm.user.Collections) { collection in
-                            CollectiomRowView(Collection: collection, editAction: {print("edit")}, DeleteActin: {print("delete")})
+                            CollectiomRowView(Collection: collection, editAction: {
+                                withAnimation() {
+                                    showEditCollection.toggle()
+                                }
+                            }, DeleteActin:{
+                                withAnimation(){
+                                    DeleteCollection.toggle()
+                                }
+                            }, ontapGesture: {selesctedCollection = collection})
                         }
                     }.padding(.horizontal)
                 }.scrollIndicators(.hidden)
                     .scrollBounceBehavior(.basedOnSize)
                     .padding(.top)
             }
-        }
+            .overlay(alignment:.bottomTrailing) {
+                Image(systemName: "plus")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 54, height: 54)
+                    .background(
+                        Circle()
+                            .fill(Color.background)
+                    )
+                    .onTapGesture {
+                        withAnimation() {
+                            showAddCollection.toggle()
+                        }
+                    }
+                    .offset(x: -20, y:-10)
+            }
+            if showAddCollection {
+                Color.black.opacity(0.6)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation() {
+                            showAddCollection = false
+                        }
+                    }
+              reuseablePopup(
+                collectionTitle: $CollectionName,
+                description: $CollectionDescriptione,
+                popupTitle: "Add New Collection",
+                titlePlaceHolder: "Collection Title",
+                testEditorPlaceHolder: "Description (write about collection)",
+                buttonAction: {},
+                buttonTitle: "Create",
+                dismiss: $showAddCollection
+              ).environmentObject(homeVm)
+                    .transition(AnyTransition.asymmetric(insertion: .scale, removal: .move(edge: .leading)))
+            }
+            if showEditCollection {
+                Color.black.opacity(0.6)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation() {
+                            showEditCollection = false
+                        }
+                    }
+              reuseablePopup(
+                collectionTitle: $CollectionNewName,
+                description: $CollectionNewDescriptione,
+                popupTitle: "Edit Your Collection",
+                titlePlaceHolder: (homeVm.user.Collections.first?.collectionTitle)!,
+                testEditorPlaceHolder: (homeVm.user.Collections.first?.collectionDescription)!,
+                buttonAction: {},
+                buttonTitle: "Save Changes",
+                dismiss: $showEditCollection
+              ).environmentObject(homeVm)
+                    .transition(AnyTransition.asymmetric(insertion:.scale, removal:.move(edge: .leading)))
+            }
+        }.navigationDestination(item: $selesctedCollection) { collection in
+            CollectionAlbumsView(Collection: collection)
+                .environmentObject(homeVm) 
+        }.fullScreenCover(isPresented: $DeleteCollection, content: {
+            reUsableBottomSheet(
+                                sheetTitle: "Delete Collection",
+                                sheetWarning: "Are you sure want to delete this collection",
+                                buttonTitle1: "cancel",
+                                buttonTitle2: "Yes,Delete",
+                                button1Action: {},
+                                button2Action: {},
+                                button1Role: .cancel,
+                                button2Role: .destructive,
+                                showSheet: $DeleteCollection
+            )
+            .background(ClearBackgroundView())
+        })
+        .environmentObject(homeVm)
     }
 }
 
@@ -47,3 +137,4 @@ struct collectionView: View {
     collectionView()
         .environmentObject(HomeViewModel())
 }
+
