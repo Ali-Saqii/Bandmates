38cd2ccfa40b6aa05f02295f4af395d88fed3a5d
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:25:19 +0500

    build view for search results

--- Bandmates/core/homeView/Views/search/searchBandmates.swift
+++ Bandmates/core/homeView/Views/search/searchBandmates.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct searchBandmates: View {
+    @EnvironmentObject var hvm : HomeViewModel
+    @State private var selecteMate: BandmateModel? = nil
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white.ignoresSafeArea()
+            ScrollView {
+                VStack {
+                    Text("Bandmates")
+                        .foregroundStyle(.black)
+                        .font(.dmSans(20, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    if !hvm.searchResults.bandmates.isEmpty {
+                        ForEach(hvm.searchResults.bandmates) { mate in
+                            OrtherUsersRowView(personImage: mate.image, PersonName: mate.fullName, personUserName: mate.userName, isRequested:mate.isRequested, buttonAction: {})
+                                .onTapGesture {
+                                    selecteMate = mate
+                                }
+                        }
+                    } else {}
+                }.padding(.horizontal)
+            }.scrollIndicators(.hidden)
+                .scrollBounceBehavior(.basedOnSize)
+                .navigationDestination(item: $selecteMate) { mate in
+                    orthersProfile(bandmate: mate)
+                        .environmentObject(hvm)
+                }
+        }
     }
 }
 
 #Preview {
     searchBandmates()
+        .environmentObject(HomeViewModel())
 }
