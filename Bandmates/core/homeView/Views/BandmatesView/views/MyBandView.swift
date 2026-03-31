c146883123e53a3b3c2f20a3b3d93f96ade07e85
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:22:13 +0500

    Build my band view

--- Bandmates/core/homeView/Views/BandmatesView/views/MyBandView.swift
+++ Bandmates/core/homeView/Views/BandmatesView/views/MyBandView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct MyBandView: View {
+    @EnvironmentObject private var Bvm : BandMatesViewModel
+    @State private var selectedPerSon: BandmateModel?  = nil
     var body: some View {
-        ScrollView {
-            Text("my Band")
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+            ScrollView {
+                VStack{
+                    if let Bandmates = Bvm.MyBandMates {
+                        ForEach(Bandmates) { mate in
+                            MyBandRowView(personImage: mate.image, fullName: mate.fullName, userName: mate.userName)
+                                .onTapGesture {
+                                    selectedPerSon = mate
+                                }
+                        }
+                    }
+                }.padding(.horizontal)
+            }.scrollIndicators(.hidden)
+                .scrollBounceBehavior(.basedOnSize)
+                .navigationDestination(item: $selectedPerSon, destination: { person in
+                    orthersProfile(bandmate: person)
+                })
+         
         }
     }
 }
 
 #Preview {
     MyBandView()
+        .environmentObject(BandMatesViewModel())
 }
