9bbd233665a6e715acfc5a733ad59a1fa7862de2
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:23:07 +0500

    build request bandmates view

--- Bandmates/core/homeView/Views/BandmatesView/views/RequestedBandmatesRowView.swift
+++ Bandmates/core/homeView/Views/BandmatesView/views/RequestedBandmatesRowView.swift
@@ -8,11 +8,85 @@
 import SwiftUI
 
 struct RequestedBandmatesRowView: View {
+    let personImage : String
+    let fullName : String
+    let userName : String
+    let DeclineButton : () -> Void
+    let ApproveButton : () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+                AsyncImage(url: URL(string: personImage)) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } else if phase.error != nil {
+                        Text(fullName.initials ?? "")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .frame(width: 53, height: 53)
+                            .background(
+                                Circle()
+                                    .fill(.secondary.opacity(0.6))
+                            )
+                    } else if personImage.isEmpty {
+                        Text(fullName.initials ?? "")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .frame(width: 53, height: 53)
+                            .background(
+                                Circle()
+                                    .fill(.secondary.opacity(0.6))
+                            )
+                    } else {
+                        ProgressView()
+                    }
+                }
+                .frame(width: 53, height: 53)
+                .clipShape(Circle())
+                .padding(.leading)
+                VStack(alignment:.leading,spacing: 6) {
+                    Text(fullName.capitalized)
+                        .font(.dmSans(14, weight: .semiBold))
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                    Text(userName)
+                        .font(.dmSans(12, weight: .medium))
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.gray)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+            HStack {
+                Button {
+                    DeclineButton()
+                } label: {
+                    Text("Decline")
+                        .foregroundStyle(.white)
+                        .font(.dmSans(10, weight: .bold))
+                        .frame(width: 64, height: 25)
+                }.background(.red)
+                    .clipShape(Capsule())
+                Button {
+                    ApproveButton()
+                } label: {
+                    Text("Approve")
+                        .foregroundStyle(.white)
+                        .font(.dmSans(10, weight: .bold))
+                        .frame(width: 64, height: 25)
+                }.background(.green)
+                    .clipShape(Capsule())
+
+            }.padding(.trailing)
+            }.frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 73)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .shadow(color: .gray.opacity(0.4), radius: 12)
+            
+        }
     }
-}
 
 #Preview {
-    RequestedBandmatesRowView()
+    RequestedBandmatesRowView(personImage: "", fullName: "", userName: "", DeclineButton: {}, ApproveButton: {})
 }
