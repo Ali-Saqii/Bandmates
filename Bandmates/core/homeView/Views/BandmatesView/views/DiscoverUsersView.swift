e61a2e6df5186f948f4ffaada0721cbe7418ce2c
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:21:35 +0500

    build discover orthers view

--- Bandmates/core/homeView/Views/BandmatesView/views/DiscoverUsersView.swift
+++ Bandmates/core/homeView/Views/BandmatesView/views/DiscoverUsersView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct DiscoverUsersView: View {
+    @EnvironmentObject var Bvm : BandMatesViewModel
+    @State private var selectedPerson: BandmateModel? = nil
     var body: some View {
-        ScrollView {
-            Text("discover")
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+            ScrollView {
+                VStack {
+                    if let users = Bvm.users {
+                        ForEach(users) { user in
+                                   OrtherUsersRowView(
+                                    personImage: user.image,
+                                    PersonName: user.fullName,
+                                    personUserName: user.userName,
+                                    isRequested: user.isRequested,
+                                       buttonAction: {
+                                           
+                                       }
+                                   ).onTapGesture {
+                                       selectedPerson = user
+                                   }
+                               }
+                    } else{}
+                }
+            }.scrollIndicators(.hidden)
+                .scrollBounceBehavior(.basedOnSize)
         }
-      
+        .navigationDestination(item: $selectedPerson, destination: { person in
+            orthersProfile(bandmate: person)
+        })
+        }
     }
-}
+
 
 #Preview {
     DiscoverUsersView()
+        .environmentObject(BandMatesViewModel())
 }
