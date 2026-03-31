9da09b8fc072c952302ed6588a53a911cc7ce468
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:21:14 +0500

    buil request bandmates view

--- Bandmates/core/homeView/Views/BandmatesView/views/BandmatesRequestView.swift
+++ Bandmates/core/homeView/Views/BandmatesView/views/BandmatesRequestView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct BandmatesRequestView: View {
+    @EnvironmentObject var Bvm : BandMatesViewModel
+    
     var body: some View {
-        ScrollView {
-            Text("request")
+        ZStack {
+            Color.white
+                .ignoresSafeArea()
+            ScrollView {
+                VStack {
+                    if let requestedMates = Bvm.requestedNandmates {
+                        ForEach(requestedMates) { mate in
+                            RequestedBandmatesRowView(personImage: mate.image, fullName: mate.fullName, userName: mate.userName, DeclineButton: {}, ApproveButton: {})
+                        }
+                    }
+                }.padding(.horizontal)
+            }.scrollIndicators(.hidden)
+                .scrollBounceBehavior(.basedOnSize)
+                .background(
+                    Color.white
+                        .ignoresSafeArea()
+                )
         }
     }
 }
-
 #Preview {
     BandmatesRequestView()
+        .environmentObject(BandMatesViewModel())
 }
