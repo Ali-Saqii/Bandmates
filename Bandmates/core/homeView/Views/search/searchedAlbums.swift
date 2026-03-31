a7514c4e6293b3f653e055583cdba8bc3901a214
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:26:46 +0500

    creating view for searched albums

--- Bandmates/core/homeView/Views/search/searchedAlbums.swift
+++ Bandmates/core/homeView/Views/search/searchedAlbums.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct searchedAlbums: View {
+    @EnvironmentObject var hvm : HomeViewModel
+    @State private var selectedAlbum : albumModel? = nil
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white.ignoresSafeArea()
+            ScrollView {
+                VStack {
+                    Text("Albums")
+                        .foregroundStyle(.black)
+                        .font(.dmSans(20, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    if !hvm.searchResults.albums.isEmpty {
+                        ForEach(hvm.searchResults.albums) { albums in
+                            AlbumsRowView(albumImage: albums.image, albumName: albums.albumName, artistName: albums.albumArtistName, ratingCount: albums.averageRating, totalRatingcount: albums.totalRatingCount, isAlBumSaved: albums.isSaved)
+                                .onTapGesture {
+                                    selectedAlbum = albums
+                                }
+                        }
+                    } else {}
+                }.padding(.horizontal)
+            }.scrollIndicators(.hidden)
+                .scrollBounceBehavior(.basedOnSize)
+        }.navigationDestination(item: $selectedAlbum) { album in
+            AlbumDetailsView(album: album)
+                .environmentObject(hvm)
+        }
     }
 }
 
 #Preview {
     searchedAlbums()
+        .environmentObject(HomeViewModel())
 }
