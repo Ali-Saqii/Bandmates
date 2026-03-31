6485dc9ee4a6a9f97bffdf7d884ec586ee4d2259
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:21:59 +0500

    build myband row view

--- Bandmates/core/homeView/Views/BandmatesView/views/MyBandRowView.swift
+++ Bandmates/core/homeView/Views/BandmatesView/views/MyBandRowView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct MyBandRowView: View {
+    let personImage : String
+    let fullName : String
+    let userName : String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+                AsyncImage(url: URL(string: personImage)) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } else if phase.error != nil {
+                        Text(fullName.initials ?? "")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .frame(width: 53, height: 53)
+                            .background(
+                                Circle()
+                                    .fill(.secondary.opacity(0.6))
+                            )
+                    } else if personImage.isEmpty {
+                        Text(fullName.initials ?? "")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .frame(width: 53, height: 53)
+                            .background(
+                                Circle()
+                                    .fill(.secondary.opacity(0.6))
+                            )
+                    } else {
+                        ProgressView()
+                    }
+                }
+                .frame(width: 53, height: 53)
+                .clipShape(Circle())
+                .padding(.leading)
+                VStack(alignment:.leading,spacing: 6) {
+                    Text(fullName.capitalized)
+                        .font(.dmSans(14, weight: .semiBold))
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                    Text(userName)
+                        .font(.dmSans(12, weight: .medium))
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.gray)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                    Image(systemName: "chevron.right")
+                        .fontWeight(.bold)
+                        .foregroundStyle(.gray)
+                        .padding(.trailing)
+            }.frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 73)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .shadow(color: .gray.opacity(0.4), radius: 12)
+            
+        }
     }
-}
 
 #Preview {
-    MyBandRowView()
+    MyBandRowView(personImage: "", fullName: "alex", userName: "@hdfbdfbdfgd")
 }
