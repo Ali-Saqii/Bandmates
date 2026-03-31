18df024822a50a84f67f99c234d08cb3622912ae
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:29:45 +0500

    notification row view

--- Bandmates/core/notifications/Views/notificationRowView.swift
+++ Bandmates/core/notifications/Views/notificationRowView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct notificationRowView: View {
+    let Notification : NotificationModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment:.top) {
+            VStack(alignment:.leading,spacing: 10) {
+                Text(Notification.title)
+                    .foregroundStyle(Notification.isRead ?.gray : .black)
+                    .font(.dmSans(15, weight: .semiBold))
+                Text(Notification.body)
+                    .foregroundStyle(Notification.isRead ?.gray : .black)
+                    .font(.dmSans(13, weight: .regular))
+                    .lineLimit(3)
+                Text("\(Notification.Date.billingFormatted)")
+                    .foregroundStyle(.gray)
+                    .font(.dmSans(14, weight: .medium))
+            }.frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+            Image(systemName:Notification.isRead ? "envelope.open" : "envelope")
+                .foregroundStyle(Notification.isRead ? Color.gray : Color.background)
+                .padding(.horizontal)
+        }.overlay(alignment: .topLeading) {
+            Circle()
+                .fill(Color.blue.opacity(Notification.isRead == true ? 0: 1))
+                .frame(width: 8, height: 8)
+        }
     }
 }
 
 #Preview {
-    notificationRowView()
+    notificationRowView(Notification:  NotificationModel(
+        title: "Collaboration Invite",
+        body: "Luna Beats invited you to collaborate on the track 'Golden Hour'.",
+        Date: Date(),
+        isRead: false
+    ))
 }
