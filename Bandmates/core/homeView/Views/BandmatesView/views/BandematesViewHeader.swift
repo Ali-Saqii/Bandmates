40c5813fc88b28f4251211b24c79092936898c6a
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-03-31 06:20:23 +0500

    build bandmates view header

--- Bandmates/core/homeView/Views/BandmatesView/views/BandematesViewHeader.swift
+++ Bandmates/core/homeView/Views/BandmatesView/views/BandematesViewHeader.swift
@@ -9,28 +9,30 @@ import SwiftUI
 
 struct BandematesViewHeader: View {
     @Binding var selectedTab: BandmatesviewTabs
-
+    
     var body: some View {
-        HStack(spacing:0) {
-            ForEach(BandmatesviewTabs.allCases, id: \.rawValue) { tab in
-                BandmatesHeaderButtons(
-                    tab:tab ,
-                    isSelected: selectedTab == tab,
-                    action: {
-                        withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
-                            selectedTab = tab
-                        }
-                    },
-                    height: 38,
-                    cornerRadius: 16)
-            }
-        }.frame(maxWidth: .infinity)
-            .frame(height: 38)
-            .background(.textfieldcolor)
-            .clipShape(RoundedRectangle(cornerRadius: 16))
-            .padding()
+       
+            HStack(spacing:0) {
+                ForEach(BandmatesviewTabs.allCases, id: \.rawValue) { tab in
+                    BandmatesHeaderButtons(
+                        tab:tab ,
+                        isSelected: selectedTab == tab,
+                        action: {
+                            withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                                selectedTab = tab
+                            }
+                        },
+                        height: 38,
+                        cornerRadius: 16)
+                }
+            }.frame(maxWidth: .infinity)
+                .frame(height: 38)
+                .background(.textfieldcolor)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .padding()
+                .background(.white)
+        }
     }
-}
 
 #Preview {
     BandematesViewHeader(selectedTab: .constant(BandmatesviewTabs.myBand))
