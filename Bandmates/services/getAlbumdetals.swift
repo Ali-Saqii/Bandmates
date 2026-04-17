778fdf3741621129b8dbebf3a7ba33303fde0273
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-17 22:34:34 +0500

    [BUILD] class for album detail service

--- Bandmates/services/getAlbumdetals.swift
+++ Bandmates/services/getAlbumdetals.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Combine
+class AlbumDetailsService {
+    
+    private let baseURL = "http://localhost:3000/user/get/albums"
+    
+    func fetchTrendingAlbums(token:String) -> AnyPublisher<[albumModel], Error> {
+        guard let url = URL(string:baseURL) else {
+            print ("bad url")
+            return Fail(error: URLError(.badURL))
+                .eraseToAnyPublisher()
+         
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+      
+        return NetworkLayer.download(request: request)
+            .decode(type: AlbumResponse.self, decoder: NetworkLayer.decoder)
+            .map{$0.data}
+            .eraseToAnyPublisher()
+      
+    }
+  
+}
