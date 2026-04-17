87abcc10af9e449672bdc32bf6e57a5a01cb8d8a
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-17 22:33:01 +0500

    [BUILD] NS presistance clsss for recently played albums

--- Bandmates/services/saveAlbums.swift
+++ Bandmates/services/saveAlbums.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import CoreData
+import Combine
+
+class savedAlbums {
+    
+    private let container : NSPersistentContainer
+    private let containerName = "savedAlbums"
+    private let entitiyName: String = "SavedAlbums"
+    @Published var savedAlbums:[SavedAlbums] = []
+    init() {
+        container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { _ , error in
+            if let error = error {
+                print("error loading core date: \(error)")
+            }
+            self.getSavedAlbums()
+        }
+        self.getSavedAlbums()
+    }
+    func getSavedAlbums() {
+        let request = NSFetchRequest<SavedAlbums>(entityName: entitiyName)
+        do {
+            savedAlbums = try container.viewContext.fetch(request)
+        } catch let error {
+            print("Error while fetching data:\(error)")
+
+        }
+    }
+    func addAlbums(album:albumModel) {
+        let entity = SavedAlbums(context: container.viewContext)
+        entity.id = album.id
+        entity.name = album.albumName
+        entity.image = album.image
+        entity.playLink = album.albumPlayLink
+        savechanges()
+    }
+    private func save() {
+        do {
+            try container.viewContext.save()
+        } catch let error {
+            print("error while saving data: \(error)")
+        }
+    }
+    private func savechanges() {
+        save()
+        getSavedAlbums()
+    }
+}
