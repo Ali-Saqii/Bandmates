5dcb2fa978482d7ea39f9da90a77b5d1877c7050
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-17 22:37:25 +0500

    name changin of model

--- Bandmates/core/collections/viewModels/collectionModel.swift
+++ Bandmates/core/collections/viewModels/collectionModel.swift
@@ -1,8 +1,14 @@
 //
-//  CollectionModel.swift
+//  collectionModel.swift
 //  Bandmates
 //
 //  Created by Mac mini on 16/04/2026.
 //
 
 import Foundation
+import Combine
+
+class collectionViewModel:ObservableObject {
+    
+    @Published var collections:[CollectionModel]? = nil
+}
