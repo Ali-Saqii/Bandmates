efe586194559f1c121bcafa5119759da8fee4f10
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-17 22:19:36 +0500

    empty view

--- Bandmates/components/contantUnaviliable Views/NoBandmatesView.swift
+++ Bandmates/components/contantUnaviliable Views/NoBandmatesView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct NoBandmatesView: View {
+    let icon: String
+    let height: CGFloat
+    let width: CGFloat
+    let title : String
+    let subTitle : String
+    let titleFont: Font
+    let subTitleFont: Font
+    let color : Color
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: icon)
+                .resizable()
+                .frame(width: width, height: height)
+                .foregroundStyle(color)
+               
+            Text(title)
+                .foregroundStyle(.black)
+                .font(titleFont)
+            Text(subTitle)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.gray)
+                .font(subTitleFont)
+                .frame(width: 270)
+
+        }
     }
 }
 
 #Preview {
-    NoBandmatesView()
+    NoBandmatesView(icon: "figure.2",height: 70,width: 100, title: "no bandmates", subTitle: "Start requesting with other music lovers to build your band!", titleFont: .dmSans(20, weight: .semiBold),subTitleFont: .dmSans(14, weight: .medium), color: Color.background)
 }
