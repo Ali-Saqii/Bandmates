2e15a84a84e3f84ee5b039adc6ea2bb18374875f
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-22 14:23:02 +0500

    integrate APi functions and Bug fixing

--- Bandmates/core/collections/Views/collectionView.swift
+++ Bandmates/core/collections/Views/collectionView.swift
@@ -19,6 +19,8 @@ struct collectionView: View {
     @State private var CollectionNewName = ""
     @State private var CollectionNewDescriptione = ""
     @State private var selesctedCollection: CollectionModel? = nil
+    
+    @State private var getId = ""
     var body: some View {
         ZStack {
             Color.white
@@ -37,19 +39,37 @@ struct collectionView: View {
                     .background(Color.textfieldcolor.opacity(0.7))
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .padding(.horizontal,20)
+                if collectionVM.isLoading && ((collectionVM.collections?.isEmpty) != nil) {
+                    ProgressView()
+                        .tint(Color.background)
+                        .padding()
+                }
+
                 ScrollView{
                     VStack(spacing:15) {
                         if let Collection = collectionVM.collections, !Collection.isEmpty {
                             ForEach(Collection) { collection in
                                 CollectiomRowView(Collection: collection, editAction: {
+                                    getId = collection.id
                                     withAnimation() {
+                                        
                                         showEditCollection.toggle()
                                     }
                                 }, DeleteActin:{
+                                    getId = collection.id
+                                    print(getId)
                                     withAnimation(){
+                                        
                                         DeleteCollection.toggle()
                                     }
                                 }, ontapGesture: {selesctedCollection = collection})
+                                .onAppear {
+                                       collectionVM.loadMoreIfNeeded(currentItem: collection) // ← pagination
+                                   }
+                            }
+                            if collectionVM.isFetchingMore {
+                                ProgressView().tint(Color.background)
+                                    .padding()
                             }
                         }else{
                             NoBandmatesView(icon: "photo.on.rectangle.angled.fill", height: 60, width: 70, title: "no collection yet".capitalized, subTitle: "Add acollection to save desire alums..", titleFont: .dmSans(18, weight: .semiBold), subTitleFont: .dmSans(18, weight: .semiBold), color: Color.background.opacity(0.6))
@@ -58,6 +78,24 @@ struct collectionView: View {
                 }.scrollIndicators(.hidden)
                     .scrollBounceBehavior(.basedOnSize)
                     .padding(.top)
+                    .onChange(of: collectionVM.successMessage) { _, newValue in
+                          if newValue != nil {
+                              DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                                  CollectionName = ""
+                                  CollectionDescriptione = ""
+                                  withAnimation() { showAddCollection = false }
+                              }
+                          }
+                      }
+                    .onChange(of: collectionVM.isUpdated) { _, newValue in
+                        if newValue {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                CollectionNewName = ""
+                                CollectionNewDescriptione = ""
+                                withAnimation() { showEditCollection = false }
+                            }
+                        }
+                    }
             }
             .overlay(alignment:.bottomTrailing) {
                 Image(systemName: "plus")
@@ -90,39 +128,64 @@ struct collectionView: View {
                 popupTitle: "Add New Collection",
                 titlePlaceHolder: "Collection Title",
                 testEditorPlaceHolder: "Description (write about collection)",
-                buttonAction: {},
+                buttonAction: {createButttonFunc()},
                 buttonTitle: "Create",
                 dismiss: $showAddCollection
-              ).environmentObject(homeVm)
+                    
+              ).overlay(content: {
+                  if collectionVM.isLoading {
+                      Rectangle()
+                          .fill(Color.textfieldcolor.opacity(0.5))
+                          .ignoresSafeArea()
+                          .overlay {
+                              ProgressView()
+                          }
+                  }
+              })
+              .environmentObject(homeVm)
+                    
                     .transition(AnyTransition.asymmetric(insertion: .scale, removal: .move(edge: .leading)))
             }
-//            if showEditCollection {
-//                Color.black.opacity(0.6)
-//                    .ignoresSafeArea()
-//                    .onTapGesture {
-//                        withAnimation() {
-//                            showEditCollection = false
-//                        }
-//                    }
-//              reuseablePopup(
-//                collectionTitle: $CollectionNewName,
-//                description: $CollectionNewDescriptione,
-//                popupTitle: "Edit Your Collection",
-//                titlePlaceHolder: (homeVm.user.albums.first?.$collectionTitle),
-//                testEditorPlaceHolder: ($homeVm.user.albums.first?.collectionDescription),
-//                buttonAction: {},
-//                buttonTitle: "Save Changes",
-//                dismiss: $showEditCollection
-//              ).environmentObject(homeVm)
-//                    .transition(AnyTransition.asymmetric(insertion:.scale, removal:.move(edge: .leading)))
-//            }
+            if showEditCollection {
+                Color.black.opacity(0.6)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation() {
+                            showEditCollection = false
+                        }
+                    }
+              reuseablePopup(
+                collectionTitle: $CollectionNewName,
+                description: $CollectionNewDescriptione,
+                popupTitle: "Edit Your Collection",
+                titlePlaceHolder: ("Enter new Title"),
+                testEditorPlaceHolder: ("enter new Description"),
+                buttonAction: {
+                    collectionVM.updateCollection(id: getId, name: CollectionNewName, description: CollectionNewDescriptione)
+                    
+                },
+                buttonTitle: "Save Changes",
+                dismiss: $showEditCollection
+              ).environmentObject(homeVm)
+                    .transition(AnyTransition.asymmetric(insertion:.scale, removal:.move(edge: .leading)))
+            }
         }.navigationDestination(item: $selesctedCollection) { collection in
             CollectionAlbumsView(Collection: collection)
-                .environmentObject(homeVm) 
-        }.fullScreenCover(isPresented: $DeleteCollection, content: {
+                .environmentObject(homeVm)
+                .environmentObject(collectionVM) 
+        }
+        
+        .fullScreenCover(isPresented: $DeleteCollection, content: {
             ZStack {
                 Color.black.opacity(0.6)
                     .ignoresSafeArea()
+                    .overlay(alignment: .top, content: {
+                        if let errormessage = collectionVM.cCErrorMessage {
+                            Text(errormessage)
+                                .foregroundStyle(.red)
+                                .font(.dmSans(14, weight: .medium))
+                        }
+                    })
                     .onTapGesture {
                         DeleteCollection = false
                     }
@@ -131,8 +194,19 @@ struct collectionView: View {
                     sheetWarning: "Are you sure want to delete this collection",
                     buttonTitle1: "cancel",
                     buttonTitle2: "Yes,Delete",
-                    button1Action: {},
-                    button2Action: {},
+                    button1Action: {
+                        withAnimation() {
+                            DeleteCollection = false
+                        }
+                    },
+                    button2Action: {
+                        collectionVM.deleteCollection(id:getId)
+                        if collectionVM.isDeleted {
+                            withAnimation() {
+                                DeleteCollection = false
+                            }
+                        }
+                    },
                     button1Role: .cancel,
                     button2Role: .destructive
                 )
@@ -140,6 +214,11 @@ struct collectionView: View {
             .background(ClearBackgroundView())
         })
         .environmentObject(homeVm)
+    }
+    
+    private func createButttonFunc () {
+        collectionVM.createCollection(title: CollectionName, description: CollectionDescriptione)
+  
     }
 }
 
