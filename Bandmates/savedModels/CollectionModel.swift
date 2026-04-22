972d9248d93066feba94eae3691f97ef5dc4a038
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-22 14:29:55 +0500

    fjghjg gfdg

--- Bandmates/savedModels/CollectionModel.swift
+++ Bandmates/savedModels/CollectionModel.swift
@@ -13,4 +13,30 @@ struct CollectionModel: Codable,Identifiable,Hashable {
     let collectionTitle: String
     let collectionDescription: String
     let albums:[albumModel]
+    
+    
+    // TO THIS:
+    enum CodingKeys: String, CodingKey {
+        case id
+        case collectionTitle                        // ← matches backend directly
+        case collectionDescription                  // ← matches backend directly
+        case albums = "savedAlbums"                 // ← keep this one
+    }
+}
+struct CollectionResponse: Codable {
+    let success: Bool
+    let message: String
+    let data: [CollectionModel]
+    let pagination: Pagination
+}
+
+struct Pagination: Codable {
+    let totalItems: Int
+    let currentPage: Int
+    let totalPages: Int
+    let perPage: Int
+}
+struct CreateCollectionBody: Encodable {
+    let name: String
+    let description: String
 }
