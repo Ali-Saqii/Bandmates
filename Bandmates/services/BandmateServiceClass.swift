763f07480a1cf18caae58fa4474aa9b543af68df
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-24 00:34:20 +0500

    Band mate service class for crud operations

--- Bandmates/services/BandmateServiceClass.swift
+++ Bandmates/services/BandmateServiceClass.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Combine
+import UIKit
+
+class BandmateClass {
+    private let baseURL = "http://localhost:3000/user"
+    
+    private var token: String {
+        UserDefaults.standard.string(forKey: "auth_token") ?? ""
+    }
+    
+    func fetchUsers(page: Int, limit: Int) -> AnyPublisher<BandmateResponse, Error> {
+
+        guard var components = URLComponents(string: "\(baseURL)/getUsers") else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+
+        components.queryItems = [
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "limit", value: "\(limit)")
+        ]
+
+        guard let url = components.url else {
+        
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        // 🔐 ADD TOKEN HERE
+        let token = UserDefaults.standard.string(forKey: "auth_token") ?? ""
+
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+
+        return NetworkLayer.download(request: request)
+            .decode(type: BandmateResponse.self, decoder: NetworkLayer.decoder)
+            .eraseToAnyPublisher()
+    }
+}
