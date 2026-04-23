97613bb7f4439370010789567c9fad98eb9d6400
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-24 00:32:56 +0500

    Adding models

--- Bandmates/savedModels/BandmateModel.swift
+++ Bandmates/savedModels/BandmateModel.swift
@@ -9,17 +9,28 @@ import Foundation
 import Combine
 
 struct BandmateModel: Codable,Identifiable, Hashable {
-    var id = UUID().uuidString
+    let id : String
     let image : String
     let fullName : String
     let userName : String
     let Bio : String
     let bandmates: Int
     let collections: Int
-    let savedCollection: [CollectionModel]
-    let BandMates: [BandmateModel]
     let isRequested : Bool
     let aretheyRequested : Bool
     let isFriend : Bool
+}
 
+
+struct pagination: Codable {
+    let totalItems: Int
+    let currentPage: Int
+    let totalPages: Int
+    let perPage: Int
+}
+
+struct BandmateResponse: Codable {
+    let success: Bool
+    let data: [BandmateModel]
+    let pagination: pagination
 }
