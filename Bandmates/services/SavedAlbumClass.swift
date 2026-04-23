5c4f1cfc367f8cb5e9858fd6e04327b6b5dc7dd0
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-24 00:34:33 +0500

    SavedAlbm class

--- Bandmates/services/SavedAlbumClass.swift
+++ Bandmates/services/SavedAlbumClass.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Combine
+
+class SaveAlbumService {
+    
+    
+    private let baseURL = "http://localhost:3000/user"
+
+    func fetchSavedAlbums(userId: String,
+                          token: String,
+                          page: Int,
+                          limit: Int) -> AnyPublisher<AlbumResponse, Error> {
+
+        guard let url = URL(string:
+            "\(baseURL)/user/savedAlbums/\(userId)?page=\(page)&limit=\(limit)"
+        ) else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        // 🔐 Auth Header
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+
+        return NetworkLayer.download(request: request)
+            .decode(type: AlbumResponse.self, decoder: NetworkLayer.decoder)
+            .eraseToAnyPublisher()
+    }
+}
