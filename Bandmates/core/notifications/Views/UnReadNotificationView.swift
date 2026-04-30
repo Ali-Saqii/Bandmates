e1c380458f0bca2ed777f63e78fae2e639e6132d
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-30 15:32:31 +0500

    UPdation

--- Bandmates/core/notifications/Views/UnReadNotificationView.swift
+++ Bandmates/core/notifications/Views/UnReadNotificationView.swift
@@ -13,22 +13,23 @@ struct UnReadNotificationView: View {
         ZStack {
             Color.white
                 .ignoresSafeArea()
-            if nvm.UnReadNotifications != nil {
-                ScrollView {
-                    if let allNotifications = nvm.UnReadNotifications {
+           
+             
+                    if !nvm.unreadNotifications.isEmpty {
+                        ScrollView {
                         VStack(spacing: 10) {
-                            ForEach(allNotifications) { notification in
+                            ForEach(nvm.unreadNotifications) { notification in
                                 notificationRowView(Notification: notification)
                                     .padding(.horizontal)
                                 Divider()
                             }
                         }
-                    }
-                }.scrollIndicators(.hidden)
-                    .scrollBounceBehavior(.basedOnSize)
-            } else {
-                NoBandmatesView(icon: "checkmark.circle.fill", height: 70, width: 70, title: "No Unreads!", subTitle: "You have no unreaded notifications notifications at the moment. Check back later for updates!", titleFont: .dmSans(20, weight: .semiBold), subTitleFont: .dmSans(14, weight: .medium), color: Color.background.opacity(0.3))
-            }
+                    }.scrollIndicators(.hidden)
+                            .scrollBounceBehavior(.basedOnSize)
+                } else {
+                    NoBandmatesView(icon: "checkmark.circle.fill", height: 70, width: 70, title: "No Unreads!", subTitle: "You have no unreaded notifications notifications at the moment. Check back later for updates!", titleFont: .dmSans(20, weight: .semiBold), subTitleFont: .dmSans(14, weight: .medium), color: Color.background.opacity(0.3))
+                }
+            
         }
     }
 }
