e2ccdd001070bf5f4f76db6d118b2770624e1892
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-30 15:34:25 +0500

    BUILD chart Models

--- Bandmates/savedModels/chartModel.swift
+++ Bandmates/savedModels/chartModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct AlbumRatingsResponse: Codable {
+    let success: Bool
+    let message: String
+    let count: Int
+    let data: [AlbumRatingModel]
+}
+
+
+struct AlbumRatingModel: Codable, Identifiable {
+    let albumId: String
+    let image: String?
+    let albumName : String?
+    let albumArtist : String?
+    let averageRating: String?
+    let ratings: RatingBreakdown
+    var id: String { albumId }
+}
+
+struct RatingBreakdown: Codable {
+    let fiveStar: Int
+    let fourStar: Int
+    let threeStar: Int
+    let twoStar: Int
+    let oneStar: Int
+}
