f29e2831905e06a0c222db0ae632ad36310082c9
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-30 15:33:00 +0500

    Bug fix

--- Bandmates/core/notifications/Views/notificationRowView.swift
+++ Bandmates/core/notifications/Views/notificationRowView.swift
@@ -8,38 +8,33 @@
 import SwiftUI
 
 struct notificationRowView: View {
-    let Notification : NotificationModel
+    let Notification : AppNotification
     var body: some View {
         HStack(alignment:.top) {
             VStack(alignment:.leading,spacing: 10) {
                 Text(Notification.title)
-                    .foregroundStyle(Notification.isRead ?.gray : .black)
+                    .foregroundStyle(Notification.is_read ?.gray : .black)
                     .font(.dmSans(15, weight: .semiBold))
                 Text(Notification.body)
-                    .foregroundStyle(Notification.isRead ?.gray : .black)
+                    .foregroundStyle(Notification.is_read ?.gray : .black)
                     .font(.dmSans(13, weight: .regular))
                     .lineLimit(3)
-                Text("\(Notification.Date.billingFormatted)")
+                Text("\(Notification.createdAt)")
                     .foregroundStyle(.gray)
                     .font(.dmSans(14, weight: .medium))
             }.frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.leading)
-            Image(systemName:Notification.isRead ? "envelope.open" : "envelope")
-                .foregroundStyle(Notification.isRead ? Color.gray : Color.background)
+            Image(systemName:Notification.is_read ? "envelope.open" : "envelope")
+                .foregroundStyle(Notification.is_read ? Color.gray : Color.background)
                 .padding(.horizontal)
         }.overlay(alignment: .topLeading) {
             Circle()
-                .fill(Color.blue.opacity(Notification.isRead == true ? 0: 1))
+                .fill(Color.blue.opacity(Notification.is_read == true ? 0: 1))
                 .frame(width: 8, height: 8)
         }
     }
 }
 
 #Preview {
-    notificationRowView(Notification:  NotificationModel(
-        title: "Collaboration Invite",
-        body: "Luna Beats invited you to collaborate on the track 'Golden Hour'.",
-        Date: Date(),
-        isRead: false
-    ))
+    notificationRowView(Notification:  AppNotification(id: "", user_id: "", type: "", title: "", body: "", is_read: false, sender_id: "", createdAt: ""))
 }
