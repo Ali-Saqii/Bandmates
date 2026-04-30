196b4a583b3dfca984705e2465c5b3e806f17bd2
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-30 15:30:14 +0500

    UPdate

---
+++ Bandmates/core/forgetPassword/views/passwordResetConfirmationView.swift
@@ -0,0 +1,38 @@
+//
+//  passwordResetConfirmationView.swift
+//  Bandmates
+//
+//  Created by Mac mini on 14/03/2026.
+//
+
+import SwiftUI
+
+struct passwordResetConfirmationView: View {
+    @Environment(\.dismiss) var dismiss
+    @Binding var showsucessSheet: Bool
+    var body: some View {
+        ZStack {
+            Color.white
+                .ignoresSafeArea(.all)
+            VStack(spacing:40) {
+                Image("Successmark")
+                Text("Bandmates has emailed your\npassword reset link.Please check your\nemail to confirm.")
+                    .multilineTextAlignment(.center)
+                    .font(.headline)
+                    .foregroundStyle(.opacity(0.8))
+                    .lineSpacing(0)
+                buttonView(action: {
+                    withAnimation(){
+                        showsucessSheet = false
+                        dismiss()
+                    }
+                }, buttonText: "Done", height: 55)
+                    .padding(.horizontal,120)
+            }
+        }
+    }
+}
+
+#Preview {
+    passwordResetConfirmationView(showsucessSheet: .constant(true))
+}
