4e58699128058cd43b446950ca2d2f57d1a964b1
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-04-30 15:35:48 +0500

    BUILD ChartSErvice class

--- Bandmates/services/albumCharService.swift
+++ Bandmates/services/albumCharService.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Combine
+
+class chartService {
+    
+    private let getChartURl = "http://localhost:3000/user/get/chart"
+
+    private var cancellables = Set<AnyCancellable>()
+
+    var token: String {
+        UserDefaults.standard.string(forKey: "auth_token") ?? ""
+    }
+    func fetchAlbumRatings() -> AnyPublisher<AlbumRatingsResponse, Error> {
+        guard let url = URL(string: getChartURl) else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        return NetworkLayer.download(request: request)
+            .handleEvents(receiveOutput: { data in
+                })
+            .decode(type: AlbumRatingsResponse.self,
+                     decoder: NetworkLayer.decoder)
+            .handleEvents(receiveOutput: { response in
+                          
+                       })
+            .eraseToAnyPublisher()
+    }
+}
